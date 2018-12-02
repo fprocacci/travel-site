cdc66c332ab313a61fb31d948f6282d46d1da7a1
Felix Procacci <felixguyprocacci@gmail.com>
2018-12-02 08:58:09 -0500

    test git

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+
+var browswerSync = require('browser-sync').create();  /* accesses only the method create */
+
+gulp.task('watch', function() {
+
+    /* Initializes a server and opens up the index.html web page in the app directory */
+        browswerSync.init({
+            notify: false,
+            server: {
+                baseDir: "app"
+            }
+        });
+    
+        watch('./app/index.html', function() {
+            browswerSync.reload(); 
+        });
+    
+        watch('./app/assets/styles/**/*.css', function() {
+            gulp.start('cssInject');
+        });
+    });
+        
+                  /*dependency functions */    
+gulp.task('cssInject', ['styles'], function() {
+    return gulp.src('./app/temp/styles/styles.css')
+        .pipe(browswerSync.stream());
+})
+    
